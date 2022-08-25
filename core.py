404629322af99be4802d2ee07150e9d2ba820578
aelamrn1 <24988856+aymannel@users.noreply.github.com>
2022-08-25 23:00:46 +0100

    exceptions accounted for

--- core.py
+++ core.py
@@ -1,36 +1,52 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
+import pandas as pd
 import requests
 import logging
 import os
+import re
 
 logging.basicConfig(level=logging.INFO, format='(%(levelname)-s) %(asctime)s %(message)s', datefmt='%d-%m %H:%M:%S')
 
 class ReactionScraper:
     def __init__(self):
-        if os.path.exists('source.html'):       # caching strategy / only need to perform HTTP request once
+        if os.path.exists('source.html'):
             logging.info('Page source via "source.html"')
-            with open('source.html') as file:
-                self.soup = BeautifulSoup(file.read(), 'lxml')
 
+            with open('source.html') as file:
+                self.soup = BeautifulSoup(file.read(), 'lxml', parse_only=SoupStrainer('a'))
         else:
             logging.info('Page source via requests')
             rq = requests.get('https://www.organic-chemistry.org/namedreactions/')
+
             try:
                 assert rq.status_code == 200
                 logging.info('HTTP request successful')
-
-                self.soup = BeautifulSoup(rq.text, 'lxml')
+                
+                self.soup = BeautifulSoup(rq.text, 'lxml', parse_only=SoupStrainer('a'))
                 with open('source.html', 'w') as file:
-                    file.write(self.soup.prettify())
-
+                    file.write(rq.text)
             except:
                 logging.error('HTTP request unsuccessful')
                 pass
 
-        self.scrape()
+        self.get_reactions()
+
+#    if link.has_attr('href'):
+#        print(link['href'])
+
 
     def get_reactions(self):
-        self.soup.
-        
+        self.reactions = dict()
+
+        for link in self.soup.select('a[href*=shtm]'):
+#            link_stripped = '\t'.join([line.strip() for line in link.text])
+
+            link_text = link.text.strip()
+            if len(link_text.splitlines()) == 1:
+                self.reactions[link_text] = link['href']
+            else:
+                reaction = ' '.join([line.strip() for line in link_text.splitlines()])
+                self.reactions[reaction] = link['href']
     
-ReactionScraper()
+rxn = ReactionScraper()
+rxn.get_reactions()
